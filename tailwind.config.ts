692ebf1c11d8a362568a497a4837763d4b7bb525
Uyoxy <blessingtimothy20171@gmail.com>
2025-01-24 11:44:52 +0100

    Added Manrope and Space Mono fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,13 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        spaceMono: ['Space Mono', 'monospace'],
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
