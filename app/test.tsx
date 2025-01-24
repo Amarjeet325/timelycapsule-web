8d539f2fe90580121a5cbfaf91321e3399dafd93
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2025-01-24 10:55:07 +0100

    test with a component

---
+++ app/test.tsx
@@ -0,0 +1,11 @@
+const TestComponent = () => {
+  return (
+    <div>
+      <p>
+        Something <span>unorganized</span> like this
+      </p>
+    </div>
+  );
+};
+
+export default TestComponent;
