d99ba927ec5cbc31364647735321ff5644c5ae5e
josephchimebuka <chimebujoseph@gmail.com>
2025-03-23 13:46:49 +0100

    resolved conflicts

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import TimeUntilUnveil from "../components/TimeUntilUnveil";
-
 export default function HomePage() {
-  const unveilDate = new Date("2025-04-01T12:00:00");
   return (
     <>
       <h1>Home Page</h1>
-      <TimeUntilUnveil unveilDate={unveilDate} />
     </>
   );
 }
--- app/components/TimeUntilUnveil.tsx
+++ app/components/TimeUntilUnveil.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+
+interface TimeUntilUnveilProps {
+  isUnveiled: boolean;
+  Days: number;
+  Minutes: number;
+  Seconds: number;
+  unveilDate: Date;
+}
 
 const getOrdinalSuffix = (day: number): string => {
   const suffixes = ["th", "st", "nd", "rd"];
   const value = day % 100;
   return suffixes[(value - 20) % 10] || suffixes[value] || suffixes[0];
 };
+
 const formatUnveilDate = (date: Date): string => {
   const day = date.getDate();
   const month = date.toLocaleString("default", { month: "long" });
@@ -13,59 +22,32 @@ const formatUnveilDate = (date: Date): string => {
   const minutes = date.getMinutes();
 
   const ordinalDay = `${day}${getOrdinalSuffix(day)}`;
-
   const formattedTime = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
   return `${ordinalDay} ${month} ${year} ${formattedTime}`;
 };
 
-const calculateTimeUntilUnveil = (unveilDate: Date): string => {
-  const now = new Date();
-  const timeDiff = unveilDate.getTime() - now.getTime();
-
-  if (timeDiff <= 0) {
-    return "Unveiled";
-  }
-
-  const days = Math.floor(timeDiff / (1000 * 3600 * 24));
-  const hours = Math.floor((timeDiff % (1000 * 3600 * 24)) / (1000 * 3600));
-  const minutes = Math.floor((timeDiff % (1000 * 3600)) / (1000 * 60));
-  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-  const formattedHours = hours < 10 ? `0${hours}` : hours;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-
-  return `${days} Days - ${formattedHours}hr:${formattedMinutes}min:${formattedSeconds}sec`;
-};
-
-interface TimeUntilUnveilProps {
-  unveilDate: Date;
-}
-
-const TimeUntilUnveil: React.FC<TimeUntilUnveilProps> = ({ unveilDate }) => {
-  const [timeRemaining, setTimeRemaining] = useState<string>(
-    calculateTimeUntilUnveil(unveilDate),
-  );
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newTimeRemaining = calculateTimeUntilUnveil(unveilDate);
-      setTimeRemaining(newTimeRemaining);
-      if (newTimeRemaining === "Unveiled") {
-        clearInterval(interval);
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [unveilDate]);
-
+const TimeUntilUnveil: React.FC<TimeUntilUnveilProps> = ({
+  isUnveiled,
+  Days,
+  Minutes,
+  Seconds,
+  unveilDate,
+}) => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="">
         <h1 className="text-sm font-bold">Countdown</h1>
-        <div className="w-[500px] p-6 rounded-lg shadow-lg flex flex-col countdownstyles border-gray-300">
+        <div
+          style={{ background: "rgba(247, 251, 253, 1)" }}
+          className="w-[500px] p-6 rounded-lg shadow-lg flex flex-col countdownstyles border-gray-300"
+        >
           <div className="flex flex-col gap-4 mb-4 border-spacing-6 border border-dashed border-gray-300 mt-4 p-6 bg-white rounded-lg">
-            <div className="text-sm font-bold text-center">{timeRemaining}</div>
+            <div className="text-sm font-bold text-center">
+              {isUnveiled
+                ? "Unveiled"
+                : `${Days} Days - ${Minutes}min:${Seconds}sec`}
+            </div>
             <div className="text-sm text-center text-gray-600">Unveil Date</div>
             <div className="text-sm text-center font-semibold text-gray-900">
               {formatUnveilDate(unveilDate)}
