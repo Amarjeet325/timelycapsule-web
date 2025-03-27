45f621809ffcb401735b1a20e6a8ababf8938c29
clintjeff2 <clintjeff2@gmail.com>
2025-03-27 19:13:27 +0100

    Imported the Capsule reusable component in the \/capsule route

--- app/(pages)/(user)/(protected)/capsules/page.tsx
+++ app/(pages)/(user)/(protected)/capsules/page.tsx
@@ -1,24 +1,11 @@
 "use client";
-import BackButton from "@/app/components/BackButton";
+import CapsulsPage from "@/app/components/CapsulePage";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex gap-4 items-center justify-center">
-        <BackButton
-          buttonAction={() => {
-            console.log("hello");
-          }}
-          variant="plain"
-        />
-        <BackButton
-          buttonAction={() => {
-            console.log("hello");
-          }}
-          variant="iconed"
-        />
-      </div>
+    <div className="w-full">
+      <CapsulsPage />
     </div>
   );
 };
